f21bd641e40ab2009fa4d1aac7ba716d35a003c7
Ferdi Sonmezay <ferdi@fsdebian.ferdisonmezay.com>
2017-01-26 17:52:03 +0300

    add gulp, copmiles scss and creates minified version

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,47 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+// source : https://markgoodyear.com/2014/01/getting-started-with-gulp/
+// Load plugins
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    rename = require('gulp-rename'),
+    notify = require('gulp-notify'),
+    del = require('del'),
+    browserSync = require('browser-sync').create();
+
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
-
+gulp.task('serve', function() {
     browserSync.init({
-        server: "./app"
+        server: "./dist"
     });
-
-    gulp.watch("app/scss/*.scss", ['sass']);
-    gulp.watch("app/*.html").on('change', browserSync.reload);
+    gulp.watch("src/styles/**/*.scss", ['styles']);
+    gulp.watch("src/styles/**/*.scss").on('change', browserSync.reload);
 });
 
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src("app/scss/*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("app/css"))
-        .pipe(browserSync.stream());
+// Styles
+gulp.task('styles', function() {
+  return sass('src/styles/main.scss', { style: 'expanded' })
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(gulp.dest('dist/styles'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(cssnano())
+    .pipe(gulp.dest('dist/styles'))
+    .pipe(notify({ message: 'Styles task complete' }));
 });
 
-gulp.task('default', ['serve']);
+// Clean
+gulp.task('clean', function() {
+  return del(['dist/styles', 'dist/scripts', 'dist/images', 'dist/*.html']);
+});
+
+gulp.task('copy', function () {
+    gulp.src('./src/*.html')
+        .pipe(gulp.dest('./dist/'))
+        .pipe(notify({ message: 'Html files have been copied' }));;
+});
+
+// Default task
+gulp.task('default', ['clean', 'copy', 'serve'], function() {
+  gulp.start('styles');
+});
